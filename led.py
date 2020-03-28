d246f49eacae99d37d10db153e203aaba02460d1
Zuzanna <zuzanna@themediatrader.com>
2020-03-28 12:34:21 +0000

    tuple out of range bc it's wrong index

--- led.py
+++ led.py
@@ -30,7 +30,7 @@ def displaydigit(digit, place):
     digits = (9,11)
     for segment in segments:
         GPIO.output(segment, GPIO.LOW)
-        GPIO.output(digits[place], digits_dict[digit][segment])
+        GPIO.output(digits[place], digits_dict[digit][segments.index(segment)])
 
 
     
