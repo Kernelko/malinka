1f96395c0be847ae73c48293902ae6bb3793ec60
Blop <zuzanna.onderko@gmail.com>
2020-03-28 18:59:06 +0000

    czas jeszcze nie najlpeszy ale dziala

--- single.py
+++ single.py
@@ -6,7 +6,7 @@ digits_dict = {
     '0': (0, 0, 0, 0, 0, 0, 1),
     '1': (1, 0, 0, 1, 1, 1, 1),
     '2': (0, 0, 1, 0, 0, 1, 0),
-    '3': (0, 0, 0, 0, 1, 1, 0),
+    '3': (0, 0, 0, 0, 0, 0, 1),
     '4': (1, 0, 0, 1, 1, 0, 0),
     '5': (0, 1, 0, 0, 1, 0, 0),
     '6': (0, 1, 0, 0, 0, 0, 0),
@@ -23,37 +23,26 @@ def setuppins():
     for segment in segments:
         GPIO.setup(segment, GPIO.OUT)
     GPIO.setup(9, GPIO.OUT)
-    GPIO.setup(11, GPIO.OUT)
 
 
 
 
 def displaydigit(digit, place):
+    digits = (9,11)
     for segment in segments:
-        if place == 0:
-            GPIO.output(9,GPIO.LOW)
-            GPIO.output(11, GPIO.HIGH)
-        elif place == 1:
-            GPIO.output(9, GPIO.HIGH)
-            GPIO.output(11, GPIO.LOW)
+        GPIO.output(9,GPIO.LOW)
         GPIO.output(segment, digits_dict[digit][segments.index(segment)])
-    
 
-def displaynumber(number):
-    t1 = datetime.now()
-    while (datetime.now()-t1).seconds <= 1:
-        displaydigit(number[0], 0)
-        time.sleep(0.005)
-        displaydigit(number[1], 1)
-        time.sleep(0.005)
-        displaydigit(number[0], 0)
+
+
+
 try:
     setuppins()
     numbers = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10']
     for number in numbers:
         print("Displaying {}".format(number))
-        displaynumber(number)
-
+        displaydigit(number[1], 1)
+        time.sleep(1)
 
 
 finally:
