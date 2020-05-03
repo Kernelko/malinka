dc523d7dbc8165b4fe5332a4a5431fee52488f9f
Blop <zuzanna.onderko@gmail.com>
2020-05-04 00:21:35 +0100

    works

--- other/weather_station/humidity.py
+++ other/weather_station/humidity.py
@@ -9,7 +9,6 @@ DHT_PIN = 2
 
 
 
-
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
     with open('data_file.csv', mode='a+') as datafile:
@@ -18,7 +17,7 @@ while True:
             date = datetime.now()
             writer.writerow([temperature, humidity,date])
             print("Temp={0:0.1f}*C Humidity= {1:0.1f}%".format(temperature, humidity))
-            time.sleep(15)
+            time.sleep(120)
         else:
             print("Failed to retrieve data")
 
